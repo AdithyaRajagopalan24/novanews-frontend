83e0a603a42d627a2d87e0448ddfd45a9c0e459e
AdithyaRajagopalan2024 <me24b002@smail.iitm.ac.in>
2026-04-02 22:12:23 +0530

    initial frontend

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const { userId } = await auth(); /
+  const { userId } = await auth();
 
   return (
     <ClerkProvider>
